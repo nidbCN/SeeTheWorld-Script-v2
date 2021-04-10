5b2696ceccbbbbc222d3e6cdda182ad15bd0f3fc
Gaein nidb <mail@gaein.cn>
2021-04-11 00:05:08 +0800

    Init project.

--- SeeTheWorld-Script-v2/Program.cs
+++ SeeTheWorld-Script-v2/Program.cs
@@ -1,2 +1,12 @@
 ﻿using System;
 
+namespace SeeTheWorld_Script_v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
