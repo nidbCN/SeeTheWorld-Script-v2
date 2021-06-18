cc2ea93abe4db2dee990b98f04408b82f4606668
Gaein nidb <Gaein_nidb_CN@outlook.com>
2021-06-18 13:55:40 +0800

    Add main method

--- SeeTheWorld-Script-v2/Program.cs
+++ SeeTheWorld-Script-v2/Program.cs
@@ -1,35 +1,49 @@
 ﻿using System;
 using System.IO;
-using System.Net.Http;
 using System.Text.Json;
+using System.Threading.Tasks;
 using SeeTheWorld_Script_v2;
 
-Uri cdnBase = new("https://img.cdn.gaein.cn/bing/");
-Uri apiBase = new("https://api.gaein.cn/SeeTheWorld/");
-
-var options = new JsonSerializerOptions()
+namespace SeeTheWorld_Script
 {
-    WriteIndented = true
-};
+    internal class Program
+    {
+        private static async Task Main()
+        {
+            Uri cdnBase = new("https://img.cdn.gaein.cn/bing/");
+            Uri apiBase = new("https://api.gaein.cn/SeeTheWorld/");
 
+            var options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
 
-const string savePath = @"/home/www-data/wwwroot/cdn/img/bing";
+            const string savePath = @"/home/www-data/wwwroot/cdn/img/bing";
 
-var httpHelper = new Helper(savePath, apiBase, cdnBase);
+            var httpHelper = new Helper(savePath, apiBase, cdnBase);
 
-Console.WriteLine("Start get picture from bing.");
-var pictureInfo = await httpHelper.GetPictureInfoAsync();
-Console.WriteLine(JsonSerializer.Serialize(pictureInfo, options));
+            Console.WriteLine("Start get picture from bing.");
+            var pictureInfo = await httpHelper.GetPictureInfoAsync();
+            Console.WriteLine(JsonSerializer.Serialize(pictureInfo, options));
 
-var fileName = DateTime.Today.ToString("yyyyMMdd") + ".jpg";
-await httpHelper.SavePictureAsync(pictureInfo, fileName);
-Console.WriteLine("Picture saved at:{0}", Path.Combine(savePath, fileName));
+            var fileName = DateTime.Today.ToString("yyyyMMdd") + ".jpg";
+            await httpHelper.SavePictureAsync(pictureInfo, fileName);
+            Console.WriteLine("Picture saved at:{0}", Path.Combine(savePath, fileName));
 
-var result = await httpHelper.AddPictureToApiAsync(pictureInfo, fileName);
-Console.WriteLine("Add picture to Api.");
-Console.WriteLine(JsonSerializer.Serialize<HttpResponseMessage>(result, options));
+            var result = await httpHelper.AddPictureToApiAsync(pictureInfo, fileName);
+            Console.WriteLine("Add picture to Api.");
+            Console.WriteLine(JsonSerializer.Serialize(new
+            {
+                StatusCode = result.StatusCode,
+                Message = result.ReasonPhrase,
+                IsSuccess = result.IsSuccessStatusCode,
+            }, options));
 
-httpHelper.FreshAliCdn(fileName);
-Console.WriteLine("AliCDN Refreshed.");
+            httpHelper.FreshAliCdn(fileName);
+            Console.WriteLine("AliCDN Refreshed.");
 
-return 0;
+        }
+    }
+
+}
+
