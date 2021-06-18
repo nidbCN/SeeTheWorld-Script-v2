77207b4596b5bafac672a323c3ab2757b588c705
Gaein nidb <Gaein_nidb_CN@outlook.com>
2021-06-18 11:41:57 +0800

    Fix bug that cannnot post picture Dto to api

--- SeeTheWorld-Script-v2/Program.cs
+++ SeeTheWorld-Script-v2/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.IO;
+using System.Net.Http;
 using System.Text.Json;
 using SeeTheWorld_Script_v2;
 
 Uri cdnBase = new("https://img.cdn.gaein.cn/Bing/");
 Uri apiBase = new("https://api.gaein.cn/SeeTheWorld/");
+
 var options = new JsonSerializerOptions()
 {
     WriteIndented = true
@@ -13,20 +15,21 @@ var options = new JsonSerializerOptions()
 
 const string savePath = @"/home/www-data/wwwroot/cdn/img/bing";
 
-var httpHelper = new Helper(savePath, apiBase);
+var httpHelper = new Helper(savePath, apiBase, cdnBase);
 
 Console.WriteLine("Start get picture from bing.");
 var pictureInfo = await httpHelper.GetPictureInfoAsync();
 Console.WriteLine(JsonSerializer.Serialize(pictureInfo, options));
 
 var fileName = DateTime.Today.ToString("yyyyMMdd") + ".jpg";
-await httpHelper.SavePictureAsync(pictureInfo, fileName);
+// await httpHelper.SavePictureAsync(pictureInfo, fileName);
 Console.WriteLine("Picture saved at:{0}", Path.Combine(savePath, fileName));
 
-await httpHelper.AddPictureToApiAsync(pictureInfo, fileName);
+var result = await httpHelper.AddPictureToApiAsync(pictureInfo, fileName);
 Console.WriteLine("Add picture to Api.");
+Console.WriteLine(JsonSerializer.Serialize<HttpResponseMessage>(result, options));
 
-httpHelper.FreshAliCdn(cdnBase, fileName);
+httpHelper.FreshAliCdn(fileName);
 Console.WriteLine("AliCDN Refreshed.");
 
 return 0;
